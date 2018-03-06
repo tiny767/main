c62cca716e2a866e6dc58d695ea78830f6dd66e5
deeheenguyen <nguyenhak22@gmail.com>
2018-03-06 10:26:41 +0800

    Add alias for AddCommand

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -47,7 +47,8 @@ public class AddressBookParser {
         final String commandWord = matcher.group("commandWord");
         final String arguments = matcher.group("arguments");
         switch (commandWord) {
-
+        case AddCommand.COMMAND_ALIAS: 
+            return new AddCommandParser().parse(arguments);
         case AddCommand.COMMAND_WORD:
             return new AddCommandParser().parse(arguments);
 
