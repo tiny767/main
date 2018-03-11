c3480353789617e78316d67b3bc10ae2ccd2e925
anhhuynh <anhhuynh98@gmail.com>
2018-03-11 22:36:12 +0800

    Fix checkstyle error.

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -1,11 +1,10 @@
 package seedu.address.logic.commands;
 
+import java.util.function.Predicate;
 import seedu.address.model.person.Person;
 
-import java.util.function.Predicate;
-
 /**
- * Finds and lists all persons in address book whose name, or email, or phone, or tags contain any of the argument keywords.
+ * Lists all persons in address book whose name, or email, or phone, or tags contain any of the argument keywords.
  * Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
@@ -13,7 +12,8 @@ public class FindCommand extends Command {
     public static final String COMMAND_WORD = "find";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names, or email, or phone"
-            + "or tags contain any of the specified keywords (case-sensitive) and displays them as a list with index numbers.\n"
+            + "or tags contain any of the specified keywords (case-sensitive) and displays them as a list with index "
+            + "numbers.\n"
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alice bob charlie";
 
