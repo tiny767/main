baf17c174a31eb06a397a75aae4fde42c49786a2
Sashankh <cksashankh@gmail.com>
2018-04-09 19:01:15 +0800

    add jobtitle and location tests

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -23,15 +23,15 @@ public class AddressTest {
     @Test
     public void isValidAddress() {
         // null address
-        Assert.assertThrows(NullPointerException.class, () -> Address.isValidAddress(null));
+        Assert.assertThrows(NullPointerException.class, () -> Address.isValidLocation(null));
 
         // invalid addresses
-        assertFalse(Address.isValidAddress("")); // empty string
-        assertFalse(Address.isValidAddress(" ")); // spaces only
+        assertFalse(Address.isValidLocation("")); // empty string
+        assertFalse(Address.isValidLocation(" ")); // spaces only
 
         // valid addresses
-        assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
-        assertTrue(Address.isValidAddress("-")); // one character
-        assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+        assertTrue(Address.isValidLocation("Blk 456, Den Road, #01-355"));
+        assertTrue(Address.isValidLocation("-")); // one character
+        assertTrue(Address.isValidLocation("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
     }
 }
