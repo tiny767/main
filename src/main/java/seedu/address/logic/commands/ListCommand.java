cab8db830a6422cadb19fa3c218af408af106913
Sashankh <cksashankh@gmail.com>
2018-04-09 15:08:21 +0800

    fix list command when list is empty

--- src/main/java/seedu/address/logic/commands/ListCommand.java
+++ src/main/java/seedu/address/logic/commands/ListCommand.java
@@ -10,11 +10,15 @@ public class ListCommand extends Command {
     public static final String COMMAND_WORD = "list";
 
     public static final String MESSAGE_SUCCESS = "Listed all persons";
-
+    public static final String MESSAGE_EMPTY_LIST = "There are no persons to list.";
 
     @Override
     public CommandResult execute() {
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
-        return new CommandResult(MESSAGE_SUCCESS);
+        if (model.getFilteredJobList().isEmpty()) {
+            return new CommandResult(MESSAGE_EMPTY_LIST);
+        } else {
+            return new CommandResult(MESSAGE_SUCCESS);
+        }
     }
 }
