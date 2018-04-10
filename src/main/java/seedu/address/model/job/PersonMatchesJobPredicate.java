8ed8a8feafee102fb7a85a381ac3f328ef9a52db
Sashankh <cksashankh@gmail.com>
2018-04-10 23:40:18 +0800

    update job matching

--- src/main/java/seedu/address/model/job/PersonMatchesJobPredicate.java
+++ src/main/java/seedu/address/model/job/PersonMatchesJobPredicate.java
@@ -9,30 +9,51 @@ import seedu.address.commons.util.StringUtil;
 import seedu.address.model.person.Person;
 
 /**
- * Tests that a {@code Person}'s {@code skills or address} matches any of the keywords given.
+ * Tests that a {@code Person}'s {@code skills, address or tags} matches any of the keywords given.
  */
 
 public class PersonMatchesJobPredicate implements Predicate<Person> {
     private final List<String> keywords;
     private final boolean notLocationBound;
+    private final boolean notTagsBound;
+    private final boolean notSkillsBound;
 
     public PersonMatchesJobPredicate(Job job) {
         this.keywords = new ArrayList<String>();
         this.keywords.addAll(Arrays.asList(job.getSkills().toString().split(",")));
         this.keywords.addAll(Arrays.asList(job.getLocation().toString().split(" ")));
 
-        notLocationBound = (job.getLocation().toString().compareTo("##") == 0);
+        notLocationBound = (job.getLocation().toString().equals("##"));
+        notTagsBound = (job.getTags().toString().equals("##"));
+        notSkillsBound = (job.getSkills().toString().equals("##"));
     }
 
     @Override
     public boolean test(Person person) {
-        String toMatchPerson = person.getAddress().toString();
-        toMatchPerson.concat(person.getSkills().toString());
-        toMatchPerson.concat(person.getTags().toString());
+        String toMatchPersonAddress = person.getAddress().toString();
+        String toMatchPersonSkills = person.getSkills().toString();
+        String toMatchPersonTags = person.getTags().toString();
 
+        boolean locationMatch =
+                keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(toMatchPersonAddress, keyword));
+        boolean skillsMatch =
+                keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(toMatchPersonSkills, keyword));
+        boolean tagsMatch =
+                keywords.stream().anyMatch(keyword -> StringUtil.containsWordIgnoreCase(toMatchPersonTags, keyword));
 
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(toMatchPerson, keyword));
+        if (notLocationBound) {
+            locationMatch = true;
+        }
+
+        if (notSkillsBound) {
+            skillsMatch = true;
+        }
+
+        if (notTagsBound) {
+            tagsMatch = true;
+        }
+
+        return locationMatch && skillsMatch && tagsMatch;
     }
 
     @Override
