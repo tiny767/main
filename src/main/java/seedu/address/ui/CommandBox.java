b0fb28781ad3e55927d72809b73d29904b374cfe
Sashankh <cksashankh@gmail.com>
2018-04-14 16:52:29 +0800

    show possible autocompletions on keypress

--- src/main/java/seedu/address/ui/CommandBox.java
+++ src/main/java/seedu/address/ui/CommandBox.java
@@ -144,7 +144,7 @@ public class CommandBox extends UiPart<Region> {
                     String.format(CommandCorrection.NO_MATCHES_FEEDBACK_TO_USER, chosenString)));
         } else {
             raise(new CommandCorrectedEvent(
-                    String.format(CommandCorrection.MATCH_FOUND_FEEDBACK_TO_USER, chosenString)));
+                    String.format(CommandCorrection.MATCH_FOUND_FEEDBACK_TO_USER, suggestions.toString())));
         }
         replaceText(chosenString);
     }
