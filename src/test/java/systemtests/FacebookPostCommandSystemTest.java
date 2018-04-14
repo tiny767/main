b1ed7d6b6501f5d908e767857a3d5b583ca339ae
tiny767 <33061312+tiny767@users.noreply.github.com>
2018-04-14 22:24:48 +0800

    Test: add system test for facebook post

---
+++ src/test/java/systemtests/FacebookPostCommandSystemTest.java
@@ -0,0 +1,78 @@
+package systemtests;
+
+import static seedu.address.commons.core.Messages.MESSAGE_UNKNOWN_COMMAND;
+
+import org.junit.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.FacebookLoginCommand;
+import seedu.address.logic.commands.FacebookPostCommand;
+import seedu.address.model.Model;
+
+public class FacebookPostCommandSystemTest extends AddressBookSystemTest {
+    @Test
+    public void facebookpost() {
+        /* ------------------------ Perform select operations on the shown unfiltered list -------------------------- */
+
+        /* Case: select the first card in the person list, command with leading spaces and trailing spaces
+         * -> selected
+         */
+        String command = "   " + FacebookPostCommand.COMMAND_WORD + " " + "some post" + " ";
+        assertCommandSuccess(command);
+
+
+
+        /* ----------------------------------- Perform invalid select operations ------------------------------------ */
+
+
+        /* Case: mixed case command word -> rejected */
+        assertCommandFailure("facebookpOst some message", MESSAGE_UNKNOWN_COMMAND);
+
+    }
+
+    /**
+     * Executes {@code command} and asserts that the,<br>
+     * 1. Command box displays an empty string.<br>
+     * 2. Command box has the default style class.<br>
+     * 3. Result display box displays the success message of executing select command with the
+     * {@code expectedSelectedCardIndex} of the selected person.<br>
+     * 4. {@code Model}, {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
+     * 5. Selected card is at {@code expectedSelectedCardIndex} and the browser url is updated accordingly.<br>
+     * 6. Status bar remains unchanged.<br>
+     * Verifications 1, 3 and 4 are performed by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     * @see AddressBookSystemTest#assertSelectedCardChanged(Index)
+     */
+    private void assertCommandSuccess(String command) {
+        Model expectedModel = getModel();
+        String expectedResultMessage = FacebookPostCommand.MESSAGE_FACEBOOK_POST_LOGIN;
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected("", expectedResultMessage, expectedModel);
+
+        assertCommandBoxShowsDefaultStyle();
+        assertStatusBarUnchanged();
+    }
+
+    /**
+     * Executes {@code command} and asserts that the,<br>
+     * 1. Command box displays {@code command}.<br>
+     * 2. Command box has the error style class.<br>
+     * 3. Result display box displays {@code expectedResultMessage}.<br>
+     * 4. {@code Model}, {@code Storage} and {@code PersonListPanel} remain unchanged.<br>
+     * 5. Browser url, selected card and status bar remain unchanged.<br>
+     * Verifications 1, 3 and 4 are performed by
+     * {@code AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)}.<br>
+     * @see AddressBookSystemTest#assertApplicationDisplaysExpected(String, String, Model)
+     */
+    private void assertCommandFailure(String command, String expectedResultMessage) {
+        Model expectedModel = getModel();
+
+        executeCommand(command);
+        assertApplicationDisplaysExpected(command, expectedResultMessage, expectedModel);
+        assertSelectedCardUnchanged();
+        assertCommandBoxShowsErrorStyle();
+        assertStatusBarUnchanged();
+    }
+}
